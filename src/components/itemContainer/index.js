cf0e493a45ad326c2a6e2c992efeef30577504c1
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2022-02-11 13:42:24 +0530

    updating mapDispatchToProps on basis of props

--- src/components/itemContainer/index.js
+++ src/components/itemContainer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyIceCream } from "../../redux/iceCream/iceCreamActions";
+import { buyCake } from "./../../redux/cake/cakeActions";
 
 const ItemContainer = (props) => {
   return (
     <div>
       <h2>Item - {props.item} </h2>
+      <button onClick={props.buyItem}>Buy Item</button>
     </div>
   );
 };
@@ -19,4 +22,14 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFucntion = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFucntion,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
